9dd042a673273fac4bfbe52561f449d7f26ba6f9
Miguel <miguel.moreira13@aluno.senai.br>
2023-08-11 16:08:16 -0300

    aula dia 10/08

--- Sprint 1/EVENT+/Banco de Dados/DQL/SQLQuery3.sql
+++ Sprint 1/EVENT+/Banco de Dados/DQL/SQLQuery3.sql
@@ -1,9 +1,31 @@
-USE [Event+]
+/*NOME DE USUARIO, TIPO DO USUARIO, DATA DO EVENTO, LOCAL DO EVENTO, TITULO DO EVENTO, NOME DO EVENTO , DESCRIÇÃO DE EVENTO, SITUAÇÃO DO EVENTO, COMENTARIO DO EVENTO*/
 
-SELECT * FROM Comentario
-SELECT * FROM Evento
-SELECT * FROM Instituicao
-SELECT * FROM PresencaEvento
-SELECT * FROM TiposdeEvento
-SELECT * FROM TiposDeUsuario
-SELECT * FROM Usuario
+SELECT
+	Usuario.Nome AS Usuário,
+	TiposDeUsuario.Título AS [Tipo de usuário],
+	Evento.DataEvento AS [Data do evento],
+	Instituicao.NomeFantasia as [Local],
+	TiposdeEvento.TituloTipoEvento AS [Tipo de evento],
+	Evento.Nome AS [Nome do evento],
+	Evento.Descricao AS [Descrição do Evento],
+	PresencaEvento.Situacao AS [Confirmação do usuario],
+	Comentario.Descricao AS [Comentario sobre o evento]
+ FROM
+	Comentario
+	INNER JOIN Usuario 
+	ON Comentario.IdUsuario = Usuario.IdUsuario
+
+	INNER JOIN TiposDeUsuario 
+	ON Usuario.IdTiposDeUsuario = TiposDeUsuario.IdTiposDeUsuario
+
+	INNER JOIN Evento 
+	ON Comentario.IdEvento = Evento.IdEvento
+
+	INNER JOIN TiposdeEvento 
+	ON Evento.IdTiposdeEvento = TiposdeEvento.IdTiposdeEvento
+
+	INNER JOIN Instituicao 
+	ON Evento.IdInstituicao = Instituicao.IdInstituicao
+
+	INNER JOIN PresencaEvento 
+	ON Usuario.IdUsuario = PresencaEvento.IdUsuario AND Evento.IdEvento = PresencaEvento.IdEvento
