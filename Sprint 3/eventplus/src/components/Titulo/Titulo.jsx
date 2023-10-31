3ca851ebc084aa8c5f1f2ccf102fb9443d9a7b24
Miguel <miguel.moreira13@aluno.senai.br>
2023-10-31 14:47:27 -0300

    adiciona projeto event+

---
+++ Sprint 3/eventplus/src/components/Titulo/Titulo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Titulo = () => {
+    return (
+        <div>
+            <h1>Titulo</h1>
+        </div>
+    );
+};
+
+export default Titulo;
---
+++ Sprint 3/eventplus/src/pages/TipoEventosPage/TipoEventos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TipoEventos.css'
+
+const TipoEventos = () => {
+    return (
+        <div>
+            <h1>Pagina de Tipos de Evento</h1>
+        </div>
+    );
+};
+
+export default TipoEventos;
