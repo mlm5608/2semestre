6f416481dab12a13ee399dc50dfcf9e7e4605afc
Miguel <miguel.moreira13@aluno.senai.br>
2023-12-01 17:04:57 -0300

    fim do login

---
+++ Sprint 3/API's locais/webapi.event+/Controllers/PresencasEventoController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.event_.Domains;
+using webapi.event_.Interfaces;
+using webapi.event_.Repositories;
+
+namespace webapi.event_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class PresencasEventoController : ControllerBase
+    {
+        private IPresencasEventoRepository _presencaEventoRepository { get; set; }
+        public PresencasEventoController()
+        {
+            _presencaEventoRepository = new PresencaRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_presencaEventoRepository.Listar());
+            }
+            catch (Exception e )
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(PresencasEvento pe)
+        {
+            try
+            {
+                _presencaEventoRepository.Inscrever(pe);
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("ListarMinhas/{id}")]
+
+        public IActionResult getMyList(Guid id)
+        {
+            try
+            {
+                
+                return Ok(_presencaEventoRepository.ListarMinhas(id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
